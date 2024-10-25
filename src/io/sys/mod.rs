d407942dfc2e23aade8983da7b0a422ca604bbcd
Eugene <usachblog@gmail.com>
2024-10-25 16:55:50 +0300

    Remove sys/windows module

--- src/io/sys/mod.rs
+++ src/io/sys/mod.rs
@@ -1,9 +1,8 @@
 //! Helper for work with the system.
 #[cfg(unix)]
 pub(crate) mod unix;
-#[cfg(windows)]
-pub(crate) mod windows;
 
+#[cfg(unix)]
 pub(crate) use io_uring::types::OpenHow;
 #[cfg(unix)]
 pub(crate) use unix::fd::*;
@@ -13,5 +12,3 @@ pub(crate) use unix::os_message_header::*;
 pub(crate) use unix::os_path as OsPath;
 #[cfg(target_os = "linux")]
 pub(crate) use unix::IOUringWorker as WorkerSys;
-#[cfg(windows)]
-pub(crate) use windows::fd::*;
